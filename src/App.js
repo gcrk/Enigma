0e0422b3ad6730d6490d8386727e6c4c8792b1e9
gcrk <g.canneton@gmail.com>
2020-12-03 22:45:56 +1100

    3rd MVP

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import End from './components/End.js'
 import Enigma1 from './Enigmas/Enigma1.js'
 import Enigma2 from './Enigmas/Enigma2.js'
 import Enigma3 from './Enigmas/Enigma3.js'
-import Enigma4 from './Enigmas/Enigma4.js'
 import Navbar from './components/Navbar.js'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -25,7 +24,7 @@ function App() {
           <Route path="/1" exact component={Enigma1} />
           <Route path="/2" exact component={Enigma2} />
           <Route path="/3" exact component={Enigma3} />
-          
+
 
         </Switch>
       </div>
